193463534a9c497dec1bef6787b767b700b020e3
Tannu Punia <officialtannu100@gmail.com>
2025-07-15 10:16:54 +0530

    new changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,10 +11,10 @@ const About = () => {
         <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
       
       <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
-        <p>Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
-        <p>Prescripto is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way.</p>
+        <p>Welcome to CarePulse, your trusted partner in managing your healthcare needs conveniently and efficiently. CarePulse , we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
+        <p>CarePulse is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, CarePulse is here to support you every step of the way.</p>
         <b className='text-gray-800'>Our Vision</b>
-        <p>Prescripto is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of th</p>
+        <p>CarePulse is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, CarePulse is here to support you every step of th</p>
       </div>
     </div>
     <div className='text-xl my-4'>
@@ -26,7 +26,7 @@ const About = () => {
    <b>Efficiency:</b>
    <p>Streamlined Appointment Scheduling That Fits Into Your Busy Lifestyle.</p>
       </div>
-      <div  className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer '>
+      <div  className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-purple-100 hover:text-black transition-all duration-300 text-gray-600 cursor-pointer '>
 <b>Convenience:</b>
 <p>Access To A Network Of Trusted Healthcare Professionals In Your Area.</p>
       </div>
